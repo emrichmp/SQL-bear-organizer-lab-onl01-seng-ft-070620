b9f1d2f6e00de54f116a16d58a2064773e65c912
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-05 21:12:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate, 4, "M")
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Gold", )
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES
