ec9f11259eed3ccbe92a058ffab4257bc62ac7bc
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-05 21:15:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Gold", "Angry", 1)
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "M", "Black", "Rowdy", 1)
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "M", "Black", "Rowdy", 1)
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "Black", "Rowdy", 0)
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "M", "Black", "Rowdy", 1)
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "M", "Black", "Rowdy", 0)
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "M", "Black", "Rowdy", 0)
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 500, "M", "Black", "Mean", 1)
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Gold", "Angry", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "M", "Black", "Rowdy", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 12, "M", "Black", "Rowdy", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "Black", "Rowdy", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "M", "Black", "Rowdy", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 12, "M", "Black", "Rowdy", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "M", "Black", "Rowdy", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 500, "M", "Black", "Mean", 1);
